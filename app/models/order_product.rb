207c12d4396fd822afb5cbe25f9d28276b37bcef
Aiste Ulozaite <aiste.ulozaite@gmail.com>
2016-03-16 21:41:44 -0700

    syntax error in models

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -2,7 +2,7 @@ class OrderProduct < ActiveRecord::Base
 
 
   belongs_to :order, inverse_of: :orderproducts
-  validates :order presence: true
+  validates :order, presence: true
 
 
   belongs_to :product, inverse_of: :orderproducts
