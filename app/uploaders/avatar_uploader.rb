9608f4321eb191443bd670f57fa6a26ed87c03fc
Aiste Ulozaite <aiste.ulozaite@gmail.com>
2016-05-11 10:37:47 -0700

    destroy button for images

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -9,8 +9,14 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
+
+
+    if Rails.env.development? || Rails.env.test?
+        storage :file
+    else
   # storage :fog
-  include CarrierWaveDirect::Uploader
+      include CarrierWaveDirect::Uploader
+    end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,15 +79,32 @@ Rails.application.configure do
 
   # Direct upload AWS S3
   CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',       # required
-    :aws_access_key_id      => ENV["AWS_KEY"],       # required
-    :aws_secret_access_key  => ENV["AWS_SECRET"],       # required
-    :region                 => 'us-west-2'  # optional, defaults to 'us-east-1'
-  }
-  config.fog_directory  = ENV["S3_BUCKET_NAME"] # required
-  # see https://github.com/jnicklas/carrierwave#using-amazon-s3
-  # for more optional configuration
-end
+    
+    config.fog_credentials = {
+      :provider               => 'AWS',       # required
+      :aws_access_key_id      => ENV["AWS_KEY"],       # required
+      :aws_secret_access_key  => ENV["AWS_SECRET"],       # required
+      :region                 => 'us-west-2'  # optional, defaults to 'us-east-1'
+    }
+    config.fog_directory  = ENV["S3_BUCKET_NAME"] # required
+    # see https://github.com/jnicklas/carrierwave#using-amazon-s3
+    # for more optional configuration
+  end
 
+  # CarrierWave.configure do |config|
+
+  #   if Rails.env.development? || Rails.env.test?
+  #     config.storage = :file
+  #   else
+  #     config.storage = :fog
+  #     config.fog_credentials = {
+  #       :provider               => 'AWS',
+  #       :aws_access_key_id      =>  ENV["AWS_KEY"],
+  #       :aws_secret_access_key  => ENV["AWS_SECRET"],
+  #       :region                 => 'us-west-2' 
+  #     }
+  #     config.fog_directory = ENV["S3_BUCKET_NAME"]
+  #   end
+
+  # end
 end
