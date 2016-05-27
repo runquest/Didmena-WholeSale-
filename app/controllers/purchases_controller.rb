e0b1b0fd95f72529ef16c6f350a2905329ee9458
Aiste Ulozaite <aiste.ulozaite@gmail.com>
2016-05-26 21:59:00 -0700

    changes to config file

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -26,7 +26,7 @@ class PurchasesController < ApplicationController
     end
 
     @purchases_for_order = Order.find(@order.id).purchases
-    Notifier.welcome_email().deliver
+    Notifier.welcome_email.deliver
     session[:cart] = nil
     redirect_to root_path
   end
