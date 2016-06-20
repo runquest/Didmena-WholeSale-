aa782e535f21c7c794ec3afc4951f7f296edb009
Aiste Ulozaite <aiste.ulozaite@gmail.com>
2016-06-19 19:03:38 -0700

    basic order confirmation email sending functionality

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,19 +56,14 @@ Rails.application.configure do
   # :enable_starttls_auto => true
   # }
 
-ActionMailer::Base.delivery_method = :smtp 
-ActionMailer::Base.smtp_settings = {
-  address: 'smtp.gmail.com', 
-  port: 587, 
-  domain: 'gmail.com',
-  authentication: 'plain', 
-  enable_starttls_auto: true, 
-  user_name: 'aiste.ulozaite@gmail.com',
-  password: 'hiwakkmrcgdmbetv'
-}
-
-
-
-
+  ActionMailer::Base.smtp_settings = {
+    address: 'smtp.gmail.com', 
+    port: 587, 
+    domain: 'gmail.com',
+    authentication: 'plain', 
+    enable_starttls_auto: true, 
+    user_name: 'aiste.ulozaite@gmail.com',
+    password: 'hiwakkmrcgdmbetv'
+  }
 
 end
