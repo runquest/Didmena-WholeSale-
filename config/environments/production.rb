7f34632bf33edfbfc819190bfd83a9f5cce48a20
runquest <runquest@users.noreply.github.com>
2016-06-25 10:44:02 -0700

    Revert "added mailer function to production"

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,32 +65,17 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
-  config.action_mailer.default_url_options = { :host => "https://serene-ridge-97774.herokuapp.com" }
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.default_url_options = { :host => "https://serene-ridge-97774.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  # SMTP settings for gmail
-  # :address              => "smtp.gmail.com",
-  # :domain               => "gmail.com",
-  # config.action_mailer.smtp_settings = {
-  #  :address              => "smtp.gmail.com",
-  #  :port                 => 587,
-  #  :domain               => "gmail.com",
-  #  :user_name            => ENV["GMAIL_USERNAME"],
-  #  :password             => ENV["GMAIL_PASSWORD"],
-  #  :authentication       => "plain",
-  # :enable_starttls_auto => true
-  # }
 
-  ActionMailer::Base.smtp_settings = {
-    address: 'smtp.gmail.com', 
-    port: 587, 
-    domain: 'gmail.com',
-    authentication: 'plain', 
-    enable_starttls_auto: true, 
-    user_name: 'aiste.ulozaite@gmail.com',
-    password: 'hiwakkmrcgdmbetv'
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+   :address              => "smtp.gmail.com",
+   :port                 => 587,
+   :user_name            => ENV["GMAIL_USERNAME"],
+   :password             => ENV["GMAIL_PASSWORD"],
+   :authentication       => "plain",
+  :enable_starttls_auto => true
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
