f92fd06e5393408e41e926b391d08c84a2326456
Aiste Ulozaite <aiste.ulozaite@gmail.com>
2016-07-15 15:27:42 -0700

    cart page - organizing models, colors under one blcok

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -43,22 +43,19 @@ class CartController < ApplicationController
     @ordered_models = Array.new;
     @ordered_products = Array.new
     @sizes = Domain.where(domain_name: "SIZE");
+    @model_products = Hash.new
+
 
     if session[:cart] then
       @cart = session[:cart]
       @cart_total_cost = 0
       @cart.each do |item|
         product = Product.find(item[0])
+        quantity = item[1]
         model = Model.find(product.model_id)
 
-        if !@ordered_models.include? model
-          @ordered_models.push(model)
-        end
-
-        if !@ordered_products.include? product
-          @ordered_products.push(product)
-        end
-
+        order_item = {"model_id" => product.model_id, "color_id" => product.color_id, "quantity" => quantity, "product" => product}
+        @ordered_products.push(order_item)
         price = model.price
         quantity = item[1].to_i
 
@@ -67,6 +64,32 @@ class CartController < ApplicationController
       end
     else
       @cart = {}
+    end
+
+    @models_array = Array.new
+    @ordered_products.each do |op|
+      if !@models_array.include? op["model_id"]
+        @models_array.push(op["model_id"])
+      end
+    end
+
+    # find all the products in module
+    @models_array_with_hashes = Array.new
+    @color_product_hash = Hash.new
+
+    @models_array.each do |model_id|
+
+      model_products_array = 
+      @ordered_products.select { |num|  num["model_id"] == model_id  }
+
+      model_colors = model_products_array.uniq{|x| x["color_id"] }
+      
+      model_colors.each do |mc|
+        model_color_array = model_products_array.select { |m| m["color_id"] == mc["color_id"] }
+        color_product = { mc["color_id"] => model_color_array}
+        @color_product_hash[mc["model_id"]] = color_product
+      end      
+
     end
   end
 end
