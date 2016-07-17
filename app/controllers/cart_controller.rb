a676a33f17d5e9e296abf22215b5a404c15d1bc8
Aiste Ulozaite <aiste.ulozaite@gmail.com>
2016-07-16 17:15:56 -0700

    changing iteration for cart index page

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -43,18 +43,37 @@ class CartController < ApplicationController
     @ordered_models = Array.new;
     @ordered_products = Array.new
     @sizes = Domain.where(domain_name: "SIZE");
+
+    # lentele atsikirai kiekvienam modeliui
+
+    # eilute butu spalva
+    # stulpelis butu dydis
+    # lastele butu kiekis
+
+    # parametrai:
+    # {"12" => "100", "34" => "10"}
+    # {"produkto_id" => "kiekis"}
+
+
+# binding.pry
+    
+    # {modelio_id => {product1, productas2, productas3}}
     @model_products = Hash.new
 
 
     if session[:cart] then
       @cart = session[:cart]
       @cart_total_cost = 0
+      
+      #per krepseli
       @cart.each do |item|
         product = Product.find(item[0])
         quantity = item[1]
+
         model = Model.find(product.model_id)
 
         order_item = {"model_id" => product.model_id, "color_id" => product.color_id, "quantity" => quantity, "product" => product}
+        # binding.pry
         @ordered_products.push(order_item)
         price = model.price
         quantity = item[1].to_i
@@ -65,7 +84,9 @@ class CartController < ApplicationController
     else
       @cart = {}
     end
-
+# binding.pry
+    
+    # Kokie yra modeliai uzsakyme
     @models_array = Array.new
     @ordered_products.each do |op|
       if !@models_array.include? op["model_id"]
@@ -77,19 +98,23 @@ class CartController < ApplicationController
     @models_array_with_hashes = Array.new
     @color_product_hash = Hash.new
 
+
     @models_array.each do |model_id|
 
-      model_products_array = 
-      @ordered_products.select { |num|  num["model_id"] == model_id  }
+      model_products_array = @ordered_products.select { 
+        |num|  num["model_id"] == model_id  
+      }
 
+      # isrenku visas spalvas tame modelyje
       model_colors = model_products_array.uniq{|x| x["color_id"] }
-      
+            # binding.pry
+
       model_colors.each do |mc|
         model_color_array = model_products_array.select { |m| m["color_id"] == mc["color_id"] }
         color_product = { mc["color_id"] => model_color_array}
         @color_product_hash[mc["model_id"]] = color_product
       end      
-
+      # binding.pry
     end
   end
 end
