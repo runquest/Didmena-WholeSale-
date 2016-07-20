af63c909f72972d31407501c26ddc67e05097d8e
Aiste Ulozaite <aiste.ulozaite@gmail.com>
2016-07-19 22:29:53 -0700

    cart index ui

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -43,7 +43,7 @@ class CartController < ApplicationController
     # @ordered_models = Array.new;
     # @ordered_products = Array.new
     @ordered_items = Array.new
-    @sizes = Domain.where(domain_name: "SIZE");
+    @sizes = Domain.where(domain_name: "SIZE").order(:id).reverse
 
     # lentele atsikirai kiekvienam modeliui
 
@@ -90,99 +90,42 @@ class CartController < ApplicationController
       @cart = {}
     end
 
-    all_models = @ordered_items.collect { |x| x["model"].title }
-    # all_models = @ordered_items.collect { |x| x["model"] }
+    all_models = @ordered_items.collect { |x| x["model"].id }
     @diff_models = all_models.uniq
-    # binding.pry
 
-    model_items = @ordered_items.select { |x| x["model"].id == 1 }
-    all_model_colors = model_items.collect { |x| x["color"].id }
-    diff_model_colors = all_model_colors.uniq
-    # binding.pry
+    @mcs_hash = Hash.new
 
-    answers = Array.new
-    # answer_lines = Array.new
+    @diff_models.each do |diff_model|
 
-    diff_model_colors.each do |model_color|
-      model_color_items = model_items.select { |x| x["color"].id ==  model_color }
-      # binding.pry
-      answer_lines = Array.new
+      model_items = @ordered_items.select { |x| x["model"].id == diff_model }
 
-      answer_lines.push(model_color)
-      @sizes.each do |s|
-        # binding.pry
-        q = "0"
-        model_color_items.each do |item|
-          if item["size"].id == s.id
-            q = item["quantity"]
-            break
+      all_model_colors = model_items.collect { |x| x["color"].id }
+      diff_model_colors = all_model_colors.uniq
+
+      answers = Array.new
+
+      diff_model_colors.each do |model_color|
+        model_color_items = model_items.select { |x| x["color"].id ==  model_color }
+        answer_lines = Array.new
+
+        answer_lines.push(model_color)
+        total_quantity = 0
+        @sizes.each do |s|
+          q = "0"
+          model_color_items.each do |item|
+            if item["size"].id == s.id
+              q = item["quantity"]
+              total_quantity += q.to_i
+              break
+            end
           end
+          answer_lines.push(q)
         end
-
-        answer_lines.push(q)
-          # if model_color_items["size"].id == s.id
-          # # if model_color_items.first["size"].id == s.id
-          #   # binding.pry
-          #   answer_lines.push(model_color_items.first["quantity"])
-          # else
-          #   answer_lines.push("0")
-          # end
-        # end
+        answer_lines.push(total_quantity)
+        answers.push(answer_lines)
       end
-      answers.push(answer_lines)
+
+      @mcs_hash[diff_model] = answers
     end
-
-    binding.pry
-
-
-    models = @ordered_items.uniq { |item| item.first } 
-    # binding.pry
-    @unique_models_array = Array.new
-    @unique_colors_array = Array.new
-
-    models.each do |model_uniq|
-      @unique_models_array.push(model_uniq["model"])
-      @unique_colors_array.push(model_uniq["colors"])
-    end
-
-
-
-    # binding.pry
-    
-    # # ordered_items
-    # # model_titles
-  
-
-
-    # # Kokie yra modeliai uzsakyme
-    # @models_array = Array.new
-    # @ordered_products.each do |op|
-    #   if !@models_array.include? op["model_id"]
-    #     @models_array.push(op["model_id"])
-    #   end
-    # end
-
-    # # find all the products in module
-    # @models_array_with_hashes = Array.new
-    # @color_product_hash = Hash.new
-
-
-    # @models_array.each do |model_id|
-
-    #   model_products_array = @ordered_products.select { 
-    #     |num|  num["model_id"] == model_id  
-    #   }
-
-    #   # isrenku visas spalvas tame modelyje
-    #   model_colors = model_products_array.uniq{|x| x["color_id"] }
-    #         # binding.pry
-
-    #   model_colors.each do |mc|
-    #     model_color_array = model_products_array.select { |m| m["color_id"] == mc["color_id"] }
-    #     color_product = { mc["color_id"] => model_color_array}
-    #     @color_product_hash[mc["model_id"]] = color_product
-    #   end      
-      # binding.pry
-    # end
   end
 end
