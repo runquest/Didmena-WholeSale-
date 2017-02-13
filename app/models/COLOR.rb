6109ddf46ddc795ab95ac633125883d936d7724c
Aiste Ulozaite <aiste.ulozaite@gmail.com>
2017-02-12 22:11:46 -0800

    Removing files

--- app/models/COLOR.rb
+++
@@ -1,3 +0,0 @@
-class Color < Domain
-  validates :code_value, format: { with: /\A\h{6}\z/, message: "only six digits or from A to F"}
-end
--- app/models/GENDER.rb
+++
@@ -1,3 +0,0 @@
-class Gender < Domain
-  validates :code_value, format: { with: /\A[WM]{1}\z/, message: "W or M"}
-end
